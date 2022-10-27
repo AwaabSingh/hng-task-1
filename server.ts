ca70bb377570ef4d2d5364414dd541081827a3f6
Awaab <adeoyin2828@gmail.com>
2022-10-27 23:48:22 +0100

    cors added

--- server.ts
+++ server.ts
@@ -1,14 +1,16 @@
-import express from "express";
-import UserRoute from './route/UserRoute'
+import express, { urlencoded } from "express";
+import UserRoute from "./route/UserRoute";
+import cors from "cors";
 
 const app = express();
 
-
 // Body parser
 app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+app.use(cors());
 
 // Hook up routes
-app.use('/api/me', UserRoute)
+app.use("/api/me", UserRoute);
 
-
-app.listen(4000, () => console.log('Server running on port 4000'));
+app.listen(4000, () => console.log("Server running on port 4000"));
